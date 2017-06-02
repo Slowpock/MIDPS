0ca307f4f1c1b6b6954a88c0c448160c65ef4e68
Admin <Slowpock>
2017-06-02 04:49:52 +0300

    Rebuld proj

---
+++ MIDPS/Lab5/Dice/app/src/main/java/com/example/dice/Diceapp.java
@@ -0,0 +1,27 @@
+package com.example.dice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Admin on 02.06.2017.
+ */
+
+public class Diceapp extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle Hinstance){
+        super.onCreate(Hinstance);
+        setContentView(R.layout.activity_dice);
+        Button Start=(Button)findViewById(R.id.Startbutton);
+        Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Diceapp.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
